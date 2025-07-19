a5e79dbd46645750fc88696bcbb64360b8977a38
paoloricciuti <ricciutipaolo@gmail.com>
2025-07-19 18:07:33 +0200

    feat: tool and resource

--- api/test.js
+++ api/test.js
@@ -1,6 +1,7 @@
 import { McpServer } from 'tmcp';
 import { ValibotJsonSchemaAdapter } from '@tmcp/adapter-valibot';
 import { HttpTransport } from '@tmcp/transport-http';
+import * as v from 'valibot';
 
 const server = new McpServer(
 	{
@@ -13,8 +14,77 @@ const server = new McpServer(
 			tools: {
 				listChanged: true,
 			},
+			resources: {
+				listChanged: true,
+			},
 		},
 		adapter: new ValibotJsonSchemaAdapter(),
+	}
+);
+
+server.tool(
+	{
+		name: 'add-numbers',
+		description: 'Adds two numbers together',
+		schema: v.object({
+			first: v.number(),
+			second: v.number(),
+		}),
+	},
+	({ first, second }) => {
+		return {
+			content: [
+				{
+					type: 'text',
+					text: `The sum of ${first} and ${second} is ${
+						first + second
+					}.`,
+				},
+			],
+		};
+	}
+);
+
+server.template(
+	{
+		name: 'products',
+		description: 'A list of products from the products API',
+		uri: 'test://products/{product}',
+		title: 'Products',
+		list: async () => {
+			const products = await fetch('https://dummyjson.com/products').then(
+				(res) => res.json()
+			);
+			const ret = products.products.map((product) => ({
+				name: product.title,
+				uri: `test://products/${product.id}`,
+				title: product.title,
+				description: product.description,
+			}));
+			return ret;
+		},
+		complete: {
+			product: async () => {
+				const products = await fetch(
+					'https://dummyjson.com/products'
+				).then((res) => res.json());
+				return products.products.map((product) => product.id);
+			},
+		},
+	},
+	async (uri, { product }) => {
+		const res = await fetch(
+			`https://dummyjson.com/products/${product}`
+		).then((res) => res.text());
+		return {
+			contents: [
+				{
+					uri,
+					text: res,
+					mimeType: 'application/json',
+				},
+			],
+		};
 	}
 );
 
